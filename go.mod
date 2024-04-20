358d836c072452df33b524ae0ac6a6f46896bd06
utya1414 <utya1414@gmail.com>
2024-04-21 02:01:43 +0900

    ユーザークエリとユニットテストを追加

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module github.com/utya1414/myblog
+module github.com/utya1414/blog-server
 
 go 1.22.1
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.9.0
 )
 
--- infrastructure/db/query/users.sql
+++ infrastructure/db/query/users.sql
@@ -1,9 +1,9 @@
--- name: GetAuthor :one
+-- name: GetUser :one
 SELECT * FROM users
-WHERE username = $1 LIMIT username;
+WHERE username = $1 LIMIT 1;
 
--- name: ListAuthors :many
-SELECT * FROM users;
+-- name: ListUsers :many
+SELECT * FROM users ORDER BY username LIMIT $1 OFFSET $2;
 
 -- name: CreateUser :one
 INSERT INTO users (
