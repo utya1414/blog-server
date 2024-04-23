b5d815710e4039af51a6553f28d4403263edecfe
utya1414 <utya1414@gmail.com>
2024-04-23 20:25:25 +0900

    prezentation層を作成し、CreateUserを追加しました。

---
+++ presentation/user/handler.go
@@ -0,0 +1,48 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/utya1414/blog-server/application/user"
+)
+
+type handler struct {
+	createUesrUseCase *user.CreateUserUseCase
+}
+
+type CreateUserParams struct {
+	Username string `json:"username"`
+	Email string	`json:"email"`
+	Password string `json:"password"`
+}
+
+func NewHandler(createUesrUseCase *user.CreateUserUseCase) *handler {
+	return &handler{createUesrUseCase: createUesrUseCase}
+}
+
+func (h *handler) CreateUser(ctx *gin.Context) {
+	var params CreateUserParams
+	if err := ctx.ShouldBindJSON(&params); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	// todo: パスワードのハッシュ化
+	err := h.createUesrUseCase.CreateUser(&user.CreateUserDto{
+		Username: params.Username,
+		Email: params.Email,
+		Password: params.Password,
+	})
+	// todo: エラーハンドリング
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "ユーザーを作成しました"})
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
