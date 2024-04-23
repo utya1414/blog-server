0030c7db5653516f3195da1967188283db696c24
utya1414 <utya1414@gmail.com>
2024-04-23 20:54:53 +0900

    add: サーバーのセットアップとapiの定義

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	userApp "github.com/utya1414/blog-server/application/user"
+	"github.com/utya1414/blog-server/infrastructure/db"
+	"github.com/utya1414/blog-server/infrastructure/repository"
+	userPre "github.com/utya1414/blog-server/presentation/user"
+)
+
+
+type Server struct {
+	
+}
+
+func NewServer(store db.Store) (*gin.Engine, error) {
+	server := setupRouter(store)
+	return server, nil
+}
+
+func setupRouter(store db.Store) *gin.Engine {
+	router := gin.Default()
+	userRep := repository.NewUserRepository(store)
+	userUse := userApp.NewCreateUserUseCase(userRep)
+	userHandler := userPre.NewHandler(userUse)
+	router.POST("/users", userHandler.CreateUser)
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	
+	return router
+}
--- main.go
+++ main.go
@@ -1,4 +1,39 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/utya1414/blog-server/api"
+	"github.com/utya1414/blog-server/infrastructure/db"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:password@localhost:5434/blog?sslmode=disable"
+	serveraddress = "0.0.0.0:8080"
+)
+
 func main() {
+	// config, err := util.LoadConfig(".")
+	// if err != nil {
+	// 	log.Fatal("cannot load config:", err)
+	// }
+	
+	conn, err := sql.Open(dbDriver, dbSource);
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	router, err := api.NewServer(store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	err = router.Run(serveraddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
